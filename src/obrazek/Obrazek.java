097497a9cb0cec7e8db38bdde65a80cf7b49dae3
Roman Auersvald <romanauersvald@seznam.cz>
2016-11-09 12:35:08 +0100

    obrazek

--- src/obrazek/Obrazek.java
+++ src/obrazek/Obrazek.java
@@ -1,5 +1,52 @@
 package obrazek;
 
-public enum Obrazek {
+import java.awt.Color;
 
+import hra.Hrac;
+import hra.HraciPlocha;
+
+public enum Obrazek {
+	HRAC("hrac", Hrac.SIRKA, Hrac.VYSKA, new Color(255,255,255)),
+	POZADI("pozadi", HraciPlocha.SIRKA*3, HraciPlocha.VYSKA, new Color(0,0,150));
+	
+	
+	//pocet prvku
+	private static final int size = Obrazek.values().length;
+	
+	//pole pro iteraci
+	private static final Obrazek[] obrazky = Obrazek.values();
+	
+	private final String klic;
+	private final int sirka;
+	private final int vyska;
+	private final Color barva;
+	
+	private Obrazek(String klic, int sirka, int vyska, Color barva){
+		this.klic = klic;
+		this.sirka = sirka;
+		this.vyska = vyska;
+		this.barva = barva;
+	}
+	
+	public String getKlic() {
+		return klic;
+	}
+	
+	public Color getBarva() {
+		return barva;
+	}
+	public int getSirka() {
+		return sirka;
+	}
+	public int getVyska() {
+		return vyska;
+	}
+	public static Obrazek[] getObrazky() {
+		return obrazky;
+	}
+	public static int getSize() {
+		return size;
+	}
+	
+	
 }
